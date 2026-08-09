7534f3b5ca58cb033bdc5a6de1eb32b38284bcdf
Pawel Palka <powersatellite@yahoo.com>
2026-08-09 08:36:40 -0500

    Reduce iOS dependency build warnings

--- expo/plugins/with-ios-pod-warnings.js
+++ expo/plugins/with-ios-pod-warnings.js
@@ -1,6 +1,20 @@
 const { withPodfile, withXcodeProject } = require('expo/config-plugins');
 
 const MARKER = '# TapMiles: keep third-party CocoaPods warnings out of the app build log';
+const POST_INSTALL_MARKER = '    # TapMiles: normalize generated Pod build settings';
+
+const POD_BUILD_SETTINGS = `${POST_INSTALL_MARKER}
+    installer.pods_project.targets.each do |target|
+      target.build_configurations.each do |build_config|
+        build_config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '15.1'
+        build_config.build_settings['OTHER_LIBTOOLFLAGS'] = '$(inherited) -no_warning_for_no_symbols'
+      end
+
+      if target.name == 'hermes-engine'
+        phase = target.shell_script_build_phases.find { |item| item.name.include?('[Hermes] Replace Hermes') }
+        phase.always_out_of_date = '1' if phase
+      end
+    end`;
 
 module.exports = function withIosPodWarnings(config) {
   config = withPodfile(config, (podfileConfig) => {
@@ -8,6 +22,12 @@ module.exports = function withIosPodWarnings(config) {
       podfileConfig.modResults.contents = podfileConfig.modResults.contents.replace(
         'prepare_react_native_project!',
         `prepare_react_native_project!\n\n${MARKER}\ninhibit_all_warnings!`
+      );
+    }
+    if (!podfileConfig.modResults.contents.includes(POST_INSTALL_MARKER)) {
+      podfileConfig.modResults.contents = podfileConfig.modResults.contents.replace(
+        /(^  post_install do \|installer\|[\s\S]*?^  end$)/m,
+        (postInstall) => postInstall.replace(/\n  end$/, `\n\n${POD_BUILD_SETTINGS}\n  end`)
       );
     }
     return podfileConfig;
@@ -18,6 +38,11 @@ module.exports = function withIosPodWarnings(config) {
     Object.values(configurations).forEach((configuration) => {
       if (configuration?.buildSettings) {
         configuration.buildSettings.ENABLE_USER_SCRIPT_SANDBOXING = 'NO';
+        configuration.buildSettings.EXTRACT_APP_INTENTS_METADATA = 'NO';
+        configuration.buildSettings.OTHER_LDFLAGS = '"$(inherited) -Wl,-no_warn_duplicate_libraries"';
+        configuration.buildSettings.OTHER_CFLAGS = '"$(inherited) -Wno-nullability-completeness -Wno-incomplete-umbrella"';
+        configuration.buildSettings.OTHER_CPLUSPLUSFLAGS = '"$(inherited) -Wno-nullability-completeness -Wno-incomplete-umbrella"';
+        configuration.buildSettings.OTHER_SWIFT_FLAGS = '"$(inherited) -Xcc -Wno-nullability-completeness -Xcc -Wno-incomplete-umbrella"';
       }
     });
     return projectConfig;
